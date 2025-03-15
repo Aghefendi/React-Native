ecd629dd1647a2169102a5adbdca5f972f8572d3
Aghefendi <erebu66@gmail.com>
2025-03-15 22:04:53 +0300

    asynawait

--- WebRequest/App.js
+++ WebRequest/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+
+ async function FetchData(){
+ const response=await axios.get('https://jsonplaceholder.typicode.com/users');
+  console.log(response);
+   
+   
+
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Fetch data' onPress={FetchData} />
     </View>
   );
 }
